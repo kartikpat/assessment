b317599c3afe76d46004cf16e17e80a57c38c0c4
patpatia <kartik@iimjobs.com>
2018-08-07 15:08:58 +0530

    added aws sqs send and receive message functionality

---
+++ app/queue/sqs/association.py
@@ -0,0 +1,39 @@
+from services import sendMessage, receiveMessage, deleteMessage
+   
+# try:
+# 	sendMessage('association keys',{
+# 	    'metaOld': { 
+# 	    	'DataType': 'Number',
+#             'StringValue': "333033" 
+#         },
+# 	    'publishedOld': { 
+# 	    	'DataType': 'Number',
+#             'StringValue': "334895"
+#         },
+# 	    'metaNew': { 
+# 	    	'DataType': 'Number',
+#             'StringValue': "123456"
+#         },
+# 	    'publishedNew': { 
+# 	    	'DataType': 'Number',
+#             'StringValue': "334896"
+#         }
+# 	})
+
+# except Exception as e:
+#     print(e)	
+
+try:
+	response = receiveMessage()
+
+	message = response['Messages'][0]
+	receipt_handle = message['ReceiptHandle']
+
+	print(message)
+
+	deleteMessage(receipt_handle)
+
+	
+
+except Exception as e:
+	print(e)	
---
+++ app/queue/sqs/services.py
@@ -0,0 +1,37 @@
+import boto3
+
+# Get the service resource
+sqs = boto3.client('sqs')
+
+queueName = 'kartiktest'
+
+response = sqs.get_queue_url(QueueName=queueName)
+
+queueURL = response['QueueUrl']
+
+def sendMessage(body, attributes):
+	response = sqs.send_message(
+					QueueUrl = queueURL,
+					MessageBody = body,
+					MessageAttributes=attributes
+				)
+
+	return response
+
+def receiveMessage():
+	response = sqs.receive_message(
+				    QueueUrl=queueURL,
+				    MaxNumberOfMessages=1,
+				    MessageAttributeNames=[
+				        'All'
+				    ],
+				    VisibilityTimeout=0
+				)
+
+	return response
+
+def deleteMessage(receiptHandle):
+	sqs.delete_message(
+		QueueUrl=queueURL,
+		ReceiptHandle=receiptHandle
+	)
